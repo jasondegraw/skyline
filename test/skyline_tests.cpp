e706c86550929b2aab6e6f758682b72f9a476ce4
Jason W. DeGraw <jason.degraw@gmail.com>
2019-08-25 18:16:22 -0600

    Update license

--- test/skyline_tests.cpp
+++ test/skyline_tests.cpp
@@ -1,4 +1,5 @@
 // Copyright (c) 2019, Alliance for Sustainable Energy, LLC
+// Copyright (c) 2019, Jason W. DeGraw
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
